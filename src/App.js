f6394fc3653aeaa34c6bb87003eeaa3b6912061b
Brady <bbbove20@gmail.com>
2019-07-08 06:52:20 -0600

    Adding palette generator.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import paletteColors from "./seedColors";
+import { generatePalette } from "./helpers/colorHelper";
 import Palette from "./components/Palette";
 
 function App() {
+  console.log(generatePalette(paletteColors[3]));
   return (
     <div className="App">
       <Palette {...paletteColors[3]} />
